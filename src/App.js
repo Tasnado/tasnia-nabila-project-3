034e9a41b1aee046388c7b0b881b354d6e794790
tasnia <tasnianabila1@gmail.com>
2021-10-03 13:48:39 -0400

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Messages from './Messages';
-// import Chat from './Chat'
 import SignOut from './SignOut';
 import { useState } from 'react';
-// import { getAuth } from "firebase/auth";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import SignIn from './SignIn';
@@ -38,38 +36,12 @@ function App() {
 
 
       <main>
-        { signedIn ? <Messages /> : <p className="signInMessage">Please Sign in</p> }
+        { signedIn ? <Messages /> : <p className="signInMessage">Please Sign in to see chat room</p> }
       </main>
 
       <footer>
         <p>Made by Tasnia Nabila :(</p>
       </footer>
-
-
-
-
-
-
-
-      {/* <header>
-        <div className="wrapper">
-          <h1>This is a heading</h1>
-          <p>I am a header</p>
-        </div>
-      </header> */}
-
-      {/* <main>
-        <div className="wrapper">
-
-          <Messages />
-          <Chat />
-
-        </div>
-      </main>
-
-      <footer>
-        <p>Made by Tasnia Nabila :(</p>
-      </footer> */}
 
     </div>
   );
