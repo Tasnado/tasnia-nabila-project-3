d2598c7522c7f78d0207e846c7801e75317ff67d
tasnia <tasnianabila1@gmail.com>
2021-10-02 21:35:22 -0400

    Sign-in and Sing-out components created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-// import Messages from './Messages';
+import Messages from './Messages';
 // import Chat from './Chat'
-// import { GoogleAuthProvider } from 'firebase/auth';
-import Auth from './Auth';
+import SignOut from './SignOut';
+import { getAuth } from "firebase/auth";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import SignIn from './SignIn';
 import './App.css';
 
 function App() {
-  // const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const user = auth.currentUser;
 
   return (
     <div className="App">
 
-      <Auth />
+
+      <header>
+        <div className="wrapper">
+          <h1>This is a heading</h1>
+          <p>I am a header</p>
+
+          { user ? <SignOut /> : <SignIn /> }
+
+        </div>
+      </header>
+
+
+
+      <main>
+        {user ? <Messages /> : <p className="signInMessage">Please Sign in</p> }
+      </main>
+
+      <footer>
+        <p>Made by Tasnia Nabila :(</p>
+      </footer>
 
 
 
 
 
 
-
-
-
-
-
-      
 
       {/* <header>
         <div className="wrapper">
